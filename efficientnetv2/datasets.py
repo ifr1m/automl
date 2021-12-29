41d4493fbc407a3d2609f60478aa058ef683cd51
Ionatan Frim <>
2021-12-29 12:18:53 +0200

    updated HyperKvasirLi3

--- efficientnetv2/datasets.py
+++ efficientnetv2/datasets.py
@@ -731,7 +731,7 @@ class HyperKvasirLi3:
             multiclass=False,
         ),
         train=dict(
-            batch_size=100,
+            batch_size=90,
             epochs=350,
             lr_base=0.016,
             lr_warmup_epoch=5,
